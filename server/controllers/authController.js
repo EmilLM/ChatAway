63052dd2be80c27f26ee6b9f0f3e313ad2cc0492
EmilLM <luchianemil@gmail>
2020-09-16 22:24:01 +0300

    heroku change

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -26,7 +26,7 @@ const createSendToken = (user, statusCode, req, res, rememberMe) => {
     const cookieOptions = {
         expires: new Date(Date.now() + maxAge),
         httpOnly: true,
-        // secure : req.secure  || req.headers('x-forward-proto') === 'https',
+        secure : req.secure  || req.headers('x-forward-proto') === 'https',
         sameSite: 'strict',
         path: '/'
     };
